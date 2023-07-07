90c7d4bd006181667430563af9f4edbb8a1da94f
Jacob Knight <78334099+jacobknighj12@users.noreply.github.com>
2023-07-07 15:14:48 +1000

    Feat: added removal of reels from facebook

--- content.js
+++ content.js
@@ -1,11 +1,22 @@
 // content.js
 
-// Function to hide the targeted elements
+// Function to hide the targeted elements on YouTube
 function hideYtdRichShelfRenderer() {
     const elements = document.querySelectorAll('ytd-rich-shelf-renderer[is-shorts]');
     elements.forEach(element => {
         element.style.display = 'none';
     });
+}
+
+// Function to remove the div with aria-label="Reels" on Facebook
+function removeFacebookReels() {
+    const reelsDiv = document.querySelector('div[aria-label="Reels"]');
+    if (reelsDiv) {
+        const parentDiv = reelsDiv.closest('div').closest('div').closest('div');
+        if (parentDiv) {
+            parentDiv.style.display = 'none';
+        }
+    }
 }
 
 // Function to remove yt-formatted-string elements containing the text "Shorts"
@@ -22,4 +33,9 @@ function removeShortsTextElements() {
 window.addEventListener('load', () => {
     hideYtdRichShelfRenderer();
     removeShortsTextElements();
+
+    // Check if on Facebook and remove Reels div and hide parent
+    if (window.location.hostname.includes('facebook.com')) {
+        removeFacebookReels();
+    }
 });
