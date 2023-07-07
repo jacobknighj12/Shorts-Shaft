c562b1fa68ea2256ac20050df41be0e06e715efe
Jacob Knight <78334099+jacobknighj12@users.noreply.github.com>
2023-07-07 15:23:32 +1000

    Feat: shorts and reels removal checked

--- content.js
+++ content.js
@@ -12,6 +12,12 @@ function hideYtdRichShelfRenderer() {
 function removeFacebookReels() {
     const reelsDiv = document.querySelector('div[aria-label="Reels"]');
     if (reelsDiv) {
+        reelsDiv.remove();
+    }
+}
+function removeFacebookReels2() {
+    const reelsDiv = document.querySelector('div[aria-label="Reels"]');
+    if (reelsDiv) {
         const parentDiv = reelsDiv.closest('div').closest('div').closest('div');
         if (parentDiv) {
             parentDiv.style.display = 'none';
@@ -29,13 +35,29 @@ function removeShortsTextElements() {
     });
 }
 
+// Mutation observer to detect changes in the DOM
+const observer = new MutationObserver(mutationsList => {
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+            // Check if on Facebook and remove Reels div
+            if (window.location.hostname.includes('facebook.com')) {
+                removeFacebookReels();
+                removeFacebookReels2();
+            }
+        }
+    }
+});
+
 // Execute the hiding and removal functions when the page finishes loading
 window.addEventListener('load', () => {
     hideYtdRichShelfRenderer();
     removeShortsTextElements();
 
-    // Check if on Facebook and remove Reels div and hide parent
+    // Check if on Facebook and remove Reels div immediately
     if (window.location.hostname.includes('facebook.com')) {
         removeFacebookReels();
     }
+
+    // Observe changes in the DOM
+    observer.observe(document.body, { childList: true, subtree: true });
 });
